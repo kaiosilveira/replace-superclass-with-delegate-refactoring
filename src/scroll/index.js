66fe0cda5f10d10c066407d679f7844d1ef3669b
Kaio Silveira <silveira.kaio@icloud.com>
2025-02-23 12:16:54 +0000

    delegate `id` to `catalogItem` at `Scroll`

--- src/scroll/index.js
+++ src/scroll/index.js
@@ -7,6 +7,10 @@ export class Scroll extends CatalogItem {
     this._lastCleaned = dateLastCleaned;
   }
 
+  get id() {
+    return this._catalogItem.id;
+  }
+
   needsCleaning(targetDate) {
     const threshold = this.hasTag('revered') ? 700 : 1500;
     return this.daysSinceLastCleaning(targetDate) > threshold;
