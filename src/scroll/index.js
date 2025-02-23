07a4ed68a49e2a61280e4ea109a48890b5b79ebd
Kaio Silveira <silveira.kaio@icloud.com>
2025-02-23 12:20:32 +0000

    remove inheritance of `CatalogItem` from `Scroll`

--- src/scroll/index.js
+++ src/scroll/index.js
@@ -1,8 +1,7 @@
 import { CatalogItem } from '../catalog/item/index.js';
 
-export class Scroll extends CatalogItem {
+export class Scroll {
   constructor(id, title, tags, dateLastCleaned) {
-    super(id, title, tags);
     this._catalogItem = new CatalogItem(id, title, tags);
     this._lastCleaned = dateLastCleaned;
   }
